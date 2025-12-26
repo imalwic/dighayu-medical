5019b21b7118a702c903fded13c206bca9d153cc
Imal <imalwic2023@gmail.com>
2025-12-27 03:45:00 +0530

    new app 5

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 import type { NextConfig } from "next";
 
-// අලුත් PWA Plugin එක සම්බන්ධ කිරීම
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
   cacheOnFrontEndNav: true,
@@ -15,21 +14,13 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  
-  // 🔥 1. Build එකේදි එන පොඩි පොඩි එරර් ගණන් ගන්න එපා කියමු
+  // Build එකේදී එන පොඩි පොඩි Error ගණන් ගන්න එපා කියමු
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-
-  // 🔥 2. Turbopack ගැටළුව විසඳන කොටස
-  experimental: {
-    turbo: {
-       // Empty object to silence the error
-    }
-  }
 };
 
 export default withPWA(nextConfig);
